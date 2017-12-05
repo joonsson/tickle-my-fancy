7b7e9a172538e3e7f9295a193ffce975eeae12a0
joonsson <joonsson@gmail.com>
2017-12-05 16:38:12 +0100

    Added working search function

--- websource/script/code.ts
+++ websource/script/code.ts
@@ -1,5 +1,21 @@
 let logo = document.getElementById("logo");
+let searchBtn = document.getElementById("searchButton");
+let searchTerm = document.getElementById("searchTerm");
 
 logo.addEventListener("click", () => {
     window.location.href = "/";
 });
+
+searchBtn.addEventListener("click", search);
+
+searchTerm.addEventListener("keypress", (e) => {
+    const key = e.which || e.keyCode;
+    if (key === 13) { // 13 is Enter
+      search();
+    }
+});
+
+function search() {
+    window.location.href = "/search?srch=" +
+    encodeURIComponent((document.getElementById("searchTerm") as HTMLInputElement).value);
+}
