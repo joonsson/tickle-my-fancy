646f1e65b490b030fa0dc14c480b1a32d1080e87
annataliveer <annataliveer@yahoo.se>
2017-12-07 14:51:22 +0100

    sound

--- websource/script/christmas.ts
+++ websource/script/christmas.ts
@@ -1,6 +1,14 @@
+const mariah = new Audio("MariahCarey.mp3");
+const fariydust = new Audio("Fairydust.mp3");
 document.addEventListener("DOMContentLoaded", (event) => {
+    mariah.play();
     const card = document.getElementsByClassName("card")[0];
     card.addEventListener("click", () => {
+        fariydust.play();
         card.classList.toggle("flipped");
     });
+});
+document.getElementById("about").addEventListener("click", () => {
+    mariah.pause();
+    document.getElementById("about").classList.add("hidden");
 });
